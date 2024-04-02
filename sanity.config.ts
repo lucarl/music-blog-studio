5b74198f2fa8f8076f054e9848b40249fcbe4397
Carl Lundborg <carl.lundborg@noaignite.com>
2024-04-02 20:32:28 +0200

    initial commit

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,20 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './schemas';
 
 export default defineConfig({
   name: 'default',
   title: 'studio',
-
   projectId: 'n0gkmc9c',
   dataset: 'production',
-
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
-})
+  cors: {
+    allowOrigins: ['https://buchlaandbeyond.com'], // Add your personal domain here
+    allowCredentials: true, // Enable credentials if needed
+    allowHeaders: ['Authorization'], // Customize allowed headers if needed
+  },
+});
